a0b64896660c06d3af69ea4a72296fc3a121454c
Nikola Anastasovski <Nikola.Anastasovski@netcetera.com>
2020-05-29 11:51:01 +0200

    Added index.html

---
+++ src/main/java/com/springmvc/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String getIndex() {
+        // what view we what rendered
+        // trazhi view shto se vika index (Ne mora da e index.html)
+        return "index";
+    }
+}
